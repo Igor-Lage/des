9eda583ea3d4073639b3f567f78208191811761d
Igor-Lage <lageigor2020@gmail.com>
2021-01-04 11:30:40 +0100

    Add files via upload

---
+++ keygen.h
@@ -0,0 +1,24 @@
+#ifndef __KEYGEN_H__
+#define __KEYGEN_H__
+
+#include <stddef.h>
+
+u_int64_t permutation(u_int64_t key, int perm);
+
+void print_bits(u_int64_t word, int range);
+
+u_int32_t rightmost_twenty_eight_bits(u_int64_t word);
+
+u_int32_t leftmost_twenty_eight_bits(u_int64_t word);
+
+u_int32_t rightmost_thirty_two_bits(u_int64_t word);
+
+u_int32_t leftmost_thirty_two_bits(u_int64_t word);
+
+u_int64_t* sixteen_key_generation(u_int64_t key);
+
+u_int32_t* sixteen_shift(u_int32_t key);
+
+u_int32_t shift(u_int32_t key);
+
+#endif
---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "fk.h"
+#include "keygen.h"
+#include "des.h"
+
+
+/* The main is here to perform simple tests to make sure everything works fine.
+ * As you can see, it generates an integer between 0 and (2^64)-1, as we were asked
+ * to work with 64-bit plaintexts. You can change it to whichever value you can, 
+ * provided it fills the requirements cited above.*/
+
+int main(){
+
+	srand(time(NULL));
+
+	u_int64_t plaintext, ciphertext;
+
+	//64-bit key
+	u_int64_t key = 0x133457799BBCDFF1;
+	plaintext = 0x123456789ABCDEF; /*rand() % 0xFFFFFFFFFFFFFFFF; //(2^64 -1)*/
+	printf("The randomly generated plaintext is ");
+	print_bits(plaintext,64);
+
+
+	// encryption of the plaintext
+	ciphertext = des_ciphertext(plaintext, key);
+	printf("\nThe ciphertext generated from the plaintext is ");
+	print_bits(ciphertext, 64);
+
+	//decryption of the ciphertext
+	plaintext = des_plaintext(ciphertext, key);
+	printf("\nThe original plaintext is ");
+	print_bits(plaintext, 64);
+
+
+
+	return(0);
+}
